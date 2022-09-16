75630ba893337693057b804392d238fe8299af66
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-09-16 20:07:43 +0200

    Add override name to the Scanner Data-Tool

--- lua/star_trek/tricorder/stools/scanner_data/shared.lua
+++ lua/star_trek/tricorder/stools/scanner_data/shared.lua
@@ -32,7 +32,7 @@ if CLIENT then
 	}
 
 	language.Add("tool.scanner_data.name", "Scanner Data-Tool")
-	language.Add("tool.scanner_data.desc", "Allows setting custom text onto an entity for retrieval using a tricorder.")
+	language.Add("tool.scanner_data.desc", "Allows setting custom text and custom name onto an entity for retrieval using a tricorder.")
 	language.Add("tool.scanner_data.left", "Set Data")
 	language.Add("tool.scanner_data.right", "Copy Data")
 	language.Add("tool.scanner_data.reload", "Delete Data")
@@ -58,11 +58,20 @@ if CLIENT then
 		local tool = ply:GetTool("scanner_data")
 		if not istable(tool) then return end
 
-		local data = textEntry:GetText()
+		local data = ""
+		if ChangeScanDataCheckBox:GetChecked() then
+			data = textEntry:GetText()
+		end
+		
+		local overrideName = ""
+		if OverrideNameCheckBox:GetChecked() then
+			overrideName = OverrideNameText:GetText()
+		end
 
 		net.Start("Scanner_Data.SetData")
 			net.WriteEntity(ent)
 			net.WriteString(data)
+			net.WriteString(overrideName)
 		net.SendToServer()
 	end)
 end
@@ -74,8 +83,10 @@ if SERVER then
 	net.Receive("Scanner_Data.SetData", function(len, ply)
 		local ent = net.ReadEntity()
 		local data = net.ReadString()
+		local overrideName = net.ReadString()
 
-		ent.ScannerData = data
+		if data ~= "" then ent.ScannerData = data end
+		ent.OverrideName = overrideName
 	end)
 
 	-- Read Custom Data from entity.
@@ -152,9 +163,35 @@ function TOOL:BuildCPanel()
 		Description = "#tool.scanner_data.desc"
 	})
 
+	OverrideNameText = vgui.Create("DTextEntry")
+	OverrideNameText:SetPlaceholderText("Enter new name (leave empty for default name)")
+
+	OverrideNameCheckBox = vgui.Create("DCheckBoxLabel")
+	OverrideNameCheckBox:SetText("Override entity's name")
+	OverrideNameCheckBox:SizeToContents()
+	OverrideNameCheckBox:SetTextColor(Color(0, 0, 0))
+	OverrideNameCheckBox:SetChecked(true)
+	function OverrideNameCheckBox:OnChange(val)
+		OverrideNameText:SetEnabled(val)
+	end
+
 	textEntry = vgui.Create("DTextEntry")
 	textEntry:SetMultiline(true)
+	textEntry:SetPlaceholderText("Enter custom data")
 	textEntry:SetSize(100, 100)
 
+	ChangeScanDataCheckBox = vgui.Create("DCheckBoxLabel")
+	ChangeScanDataCheckBox:SetText("Set custom scan data")
+	ChangeScanDataCheckBox:SizeToContents()
+	ChangeScanDataCheckBox:SetTextColor(Color(0, 0, 0))
+	ChangeScanDataCheckBox:SetChecked(true)
+	function ChangeScanDataCheckBox:OnChange(val)
+		textEntry:SetEnabled(val)
+	end
+
+
+	self:AddItem(OverrideNameCheckBox)
+	self:AddItem(OverrideNameText)
+	self:AddItem(ChangeScanDataCheckBox)
 	self:AddItem(textEntry)
 end
